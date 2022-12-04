3ade1c715c63e6f90186b598f2cee374a4a30b9f
stelko <stilyanmtoskov@gmail.com>
2022-12-04 21:39:49 +0000

    model associations

--- models/course.js
+++ models/course.js
@@ -1,26 +1,27 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
-  class Course extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Course.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    materialsNeeded: DataTypes.STRING,
-    userId: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Course',
-  });
+  class Course extends Model {}
+  Course.init(
+    {
+      title: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      materialsNeeded: DataTypes.STRING,
+      userId: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: 'Course',
+    },
+  );
+  Course.associate = (models) => {
+    Course.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'userId',
+    });
+  };
+
   return Course;
 };
